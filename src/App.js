c0ff788328dd854b7d59a7c34207ec66fce3059a
whgkswo <gjuk7238@gmail.com>
2024-08-15 19:00:55 +0900

    first page completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import './style/MainPageWeb.css'
 import { useState } from 'react';
-import Article from './components/Article';
 
 function Modal(){
   return(
@@ -18,47 +17,87 @@ function Modal(){
 
 function Header(){
   return (
-    <div className='black-nav'>
-        <h4>블로그임</h4>
+    <div className='header'>
+        <img src='meetbti.png'></img>
+        <div id='menu-container'>
+          <h2>MBTI 테스트</h2>
+          <h2>MY MBTI</h2>
+          <h2>Snack</h2>
+        </div>
+        <img src='mypage-icon.png' id='mypage-icon'></img>
     </div>
+  );
+}
+function MainBanner(){
+  return (
+    <div className='Main'>
+      <img src='main.png' id='main-banner'/>
+
+      <div className='main-text'>
+        <div className='main-large'>
+          <div>나만의 MBTI</div>
+          <div>최근 나의 성격은</div>
+        </div>
+        <div className='main-small'>
+          <div>나의 MBTI는,</div>
+          <div>너의 MBTI는,</div>
+          <div>우리의 MBTI는</div>
+          <button className='test-start'>START!</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+function MainContents(){
+  return (
+    <div className='main-contents'>
+      <h6>나와 같은 Meet MBTI!</h6>
+    </div>
+  );
+}
+function ImageContainer(){
+  return (
+    <div className='image-container'>
+      <img src='person-1.jpg' className='main-person'></img>
+      <img src='person-2.jpg' className='main-person'></img>
+      <img src='person-3.jpg' className='main-person'></img>
+      <img src='person-4.jpg' className='main-person'></img>
+      <img src='person-5.jpg' className='main-person'></img>
+    </div>
+  );
+}
+function FooterList(props){
+  return (
+    <div>
+      <h4>{props.title}</h4>
+      <div>
+        {props.content.map((item, index) => (
+        <div key={index}>{item}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+function MainFooter(){
+  return (
+    <footer className='main-footer'>
+      <div className='footer-container'>
+        <FooterList title='Use cases' content={['UI design', 'UX desingn', 'Wireframing']}></FooterList>
+        <FooterList title='Explore' content={['Design', 'Prototyping', 'Development features']}></FooterList>
+        <FooterList title='Resources' content={['Blog', 'Best practices', 'Colors']}></FooterList>
+      </div>
+    </footer>
   );
 }
 
 function App(){
-
-
-  function sort(){
-    const sortedArticles = [...articles].sort((a,b) => a.title.localeCompare(b.title));
-    setArticles(sortedArticles);
-  }
-
-  const [articles, setArticles] = useState(
-    [
-      {title:'남자코트 추천', content:'2월 17일 발행', likes:0},
-      {title:'강남 우동맛집', content:'2월 18일 발행', likes:0},
-      {title:'파이썬독학', content:'2월 19일 발행', likes:0}
-    ]
-  );
-
-  function addLike(index){
-    const copy = [...articles];
-    copy[index].likes += 1;
-    setArticles(copy);
-  }
-
   return(
     <div className="App">
       <Header></Header>
-
-      {articles.map((item, index) => (
-        <Article 
-          title={item.title}
-          content={item.content}
-          likes={item.likes}
-          addLike={() => addLike(index)}>
-        </Article>
-      ))}
-      <button onClick={sort}>글 정렬</button>
+      <MainBanner></MainBanner>
+      <MainContents></MainContents>
+      <ImageContainer></ImageContainer>
+      <MainFooter></MainFooter>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+function Login(){
+    return (
+        <div>
+            <input type="text" placeholder="ID"></input>
+            <input type="text" placeholder="password"></input>
+            <button>Login</button>
+            <label><input type="checkbox"/>Keep Login</label>
+        </div>
+    );
+}
+export default Login;
